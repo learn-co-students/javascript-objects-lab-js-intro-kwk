95b4191b1d9f672f61724c71eeebe9cfa742caad
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-12-27 16:29:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {prop: 1}; 
+
+function updateObjectWithKeyAndValue(object, key, value) {
+object[key] = value
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  recipes.prop2 = 2; 
+  return recipes 
+}
+
+function deleteFromObjectByKey(recipes, key) {
+  delete recipes.prop2;
+  return recipes 
+}
+
+function destructivelyDeleteFromObjectByKey(recipes, key) {
+  delete recipes.prop2;
+  return recipes 
+}
